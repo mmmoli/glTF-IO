e30ff910244cb4cc3aed674cd87ccd8bfbebacd8
visose <vicente@visose.com>
2022-11-27 17:17:07 +0000

    add library project

---
+++ Rhino.glTF/Convert.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Rhino.DocObjects;
+using glTF_BinExporter;
+
+namespace Rhino.glTF;
+
+public static class Convert
+{
+    public static byte[] ToGlb(glTFExportOptions options, IEnumerable<RhinoObject> objects)
+    {
+        var doc = objects.FirstOrDefault()?.Document;
+
+        if (doc is null)
+            return new byte[0];
+
+        var workflow = doc.RenderSettings.LinearWorkflow;
+        RhinoDocGltfConverter converter = new(options, true, doc, objects, workflow);
+        glTFLoader.Schema.Gltf gltf = converter.ConvertToGltf();
+
+        byte[] bytes = converter.GetBinaryBuffer();
+        using MemoryStream stream = new();
+        glTFLoader.Interface.SaveBinaryModel(gltf, bytes.Length == 0 ? null : bytes, stream);
+        return stream.ToArray();
+    }
+}
---
+++ Rhino.glTF/glTFExportOptions.cs
@@ -0,0 +1,31 @@
+﻿
+namespace glTF_BinExporter;
+
+public enum SubDMode : int
+{
+    ControlNet = 0,
+    Surface = 1,
+}
+
+public class glTFExportOptions
+{
+    public bool MapRhinoZToGltfY { get; set; } = true;
+    public bool ExportMaterials { get; set; } = true;
+    public bool UseDisplayColorForUnsetMaterials { get; set; } = true;
+
+    public SubDMode SubDExportMode { get; set; } = SubDMode.Surface;
+    public int SubDLevel { get; set; } = 4;
+
+    public bool ExportTextureCoordinates { get; set; } = true;
+    public bool ExportVertexNormals { get; set; } = true;
+    public bool ExportOpenMeshes { get; set; } = true;
+    public bool ExportVertexColors { get; set; } = false;
+
+    public bool UseDracoCompression { get; set; } = false;
+    public int DracoCompressionLevel { get; set; } = 10;
+    public int DracoQuantizationBitsPosition { get; set; } = 11;
+    public int DracoQuantizationBitsNormal { get; set; } = 8;
+    public int DracoQuantizationBitsTexture { get; set; } = 10;
+
+    public bool ExportLayers { get; set; }
+}
